f84fa7e94f9049697e705e220ae920c116501123
QcloudApi <QcloudApi@tencent.com>
2015-08-11 17:11:08 +0800

    add Wenzhi.cs

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Wenzhi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Wenzhi : Base
+    {
+        public Wenzhi()
+        {
+            serverHost = "wenzhi.api.qcloud.com";
+        }
+    }
+}
