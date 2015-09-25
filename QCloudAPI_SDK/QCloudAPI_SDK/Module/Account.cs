3eb9e6874047dd79dccd2cfd5494ec93994d135a
QcloudApi <QcloudApi@users.noreply.github.com>
2015-09-25 15:54:01 +0800

    添加 Account 模块

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Account.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Account : Base
+    {
+        public Account()
+        {
+            serverHost = "account.api.qcloud.com";
+        }
+    }
+}
