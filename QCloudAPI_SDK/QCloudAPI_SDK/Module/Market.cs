131a5764854683ec9f3f4db401097109a97de6d9
QcloudApi <QcloudApi@users.noreply.github.com>
2015-10-16 20:43:10 +0800

    添加Market.cs

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Market.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Market : Base
+    {
+        public Market()
+        {
+            serverHost = "market.api.qcloud.com";
+        }
+    }
+}
