81255b9329121501b25c5bba80efab6aa0007dd7
QcloudApi <QcloudApi@users.noreply.github.com>
2015-11-05 16:32:49 +0800

    Create Bill.cs
    添加Bill模块

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Bill.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Bill : Base
+    {
+        public Bill()
+        {
+            serverHost = "bill.api.qcloud.com";
+        }
+    }
+}
