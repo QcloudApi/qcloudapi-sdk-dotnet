5965885d9c7e181e5d3a72f8b29fb1179dff4cfb
QcloudApi <QcloudApi@users.noreply.github.com>
2015-11-05 19:19:53 +0800

    新增vpc模块

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Vpc.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Vpc : Base
+    {
+        public Vpc()
+        {
+            serverHost = "vpc.api.qcloud.com";
+        }
+    }
+}
