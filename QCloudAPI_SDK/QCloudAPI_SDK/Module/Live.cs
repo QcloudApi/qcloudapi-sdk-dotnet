008ba7d443d45e61fa2775a7ac2e0854e390f6d7
QcloudApi <QcloudApi@users.noreply.github.com>
2015-12-11 20:53:57 +0800

    添加live模块

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Live.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Live : Base
+    {
+        public Live()
+        {
+            serverHost = "live.api.qcloud.com";
+        }
+    }
+}
