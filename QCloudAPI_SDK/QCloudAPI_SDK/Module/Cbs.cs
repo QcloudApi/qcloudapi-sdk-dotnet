5663fb7c3d5aec09c82233e5a4607ba92e57e975
QcloudApi <QcloudApi@users.noreply.github.com>
2016-05-26 09:36:19 +0800

    添加Cbs模块

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Cbs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Cbs : Base
+    {
+        public Cbs()
+        {
+            serverHost = "cbs.api.qcloud.com";
+        }
+    }
+}
