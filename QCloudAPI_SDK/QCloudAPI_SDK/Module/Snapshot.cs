4b5358b26b3b7316cdde79b31e7c7f7493238fd5
QcloudApi <QcloudApi@users.noreply.github.com>
2016-05-26 09:37:08 +0800

    添加Snapshot模块

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Snapshot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Snapshot : Base
+    {
+        public Snapshot()
+        {
+            serverHost = "snapshot.api.qcloud.com";
+        }
+    }
+}
