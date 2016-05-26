f9b8b530920d87a0e15ff2dee22b854eaf198d02
QcloudApi <QcloudApi@users.noreply.github.com>
2016-05-26 09:36:41 +0800

    添加Scaling模块

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Scaling.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Scaling : Base
+    {
+        public Scaling()
+        {
+            serverHost = "scaling.api.qcloud.com";
+        }
+    }
+}
