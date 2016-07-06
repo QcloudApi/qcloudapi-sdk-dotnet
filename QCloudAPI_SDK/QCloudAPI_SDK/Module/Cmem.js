552f82f66375789b14424dbd60240214a92de520
QcloudApi <QcloudApi@users.noreply.github.com>
2016-07-06 18:47:16 +0800

    增加Cmem模块

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Cmem.js
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Cmem : Base
+    {
+        public Cmem()
+        {
+            serverHost = "cmem.api.qcloud.com";
+        }
+    }
+}
