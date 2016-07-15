72eb9ab963c96ae60a3d8e310f7be581d83f62d0
QcloudApi <QcloudApi@users.noreply.github.com>
2016-07-15 21:13:59 +0800

    Create Tdsql.cs

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Tdsql.cs
@@ -0,0 +1,15 @@
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Tdsql : Base
+    {
+        public Tdsql()
+        {
+            serverHost = "tdsql.api.qcloud.com";
+        }
+    }
+}
