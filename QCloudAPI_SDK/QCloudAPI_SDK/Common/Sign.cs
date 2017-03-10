88b26824811fbb9ddc48c25a8f7f8e682d870735
QcloudApi <QcloudApi@users.noreply.github.com>
2017-03-10 10:37:24 +0800

    兼容HmacSHA256算法
    兼容HmacSHA256算法

--- QCloudAPI_SDK/QCloudAPI_SDK/Common/Sign.cs
+++ QCloudAPI_SDK/QCloudAPI_SDK/Common/Sign.cs
@@ -16,11 +16,18 @@ namespace QCloudAPI_SDK.Common
         ///<returns>签名</returns>
         public static string Signature(string signStr, string secret)
         {
-            using (HMACSHA1 mac = new HMACSHA1(Encoding.UTF8.GetBytes(secret)))
-            {
-                byte[] hash = mac.ComputeHash(Encoding.UTF8.GetBytes(signStr));
-                return Convert.ToBase64String(hash);
-            }
+            if (SignatureMethod == "HmacSHA256")
+                using (HMACSHA256 mac = new HMACSHA256(Encoding.UTF8.GetBytes(secret)))
+                {
+                    byte[] hash = mac.ComputeHash(Encoding.UTF8.GetBytes(signStr));
+                    return Convert.ToBase64String(hash);
+                }
+            else
+                using (HMACSHA1 mac = new HMACSHA1(Encoding.UTF8.GetBytes(secret)))
+                {
+                    byte[] hash = mac.ComputeHash(Encoding.UTF8.GetBytes(signStr));
+                    return Convert.ToBase64String(hash);
+                }
         }
 
         protected static string BuildParamStr(SortedDictionary<string, object> requestParams, string requestMethod = "GET")
