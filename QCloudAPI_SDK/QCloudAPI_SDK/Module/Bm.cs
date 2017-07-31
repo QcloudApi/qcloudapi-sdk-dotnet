8c6c667458b4c2b2b6c778c2d594d124e85050cb
jimmyzhuang <jimmyzhuang@tencent.com>
2017-07-31 20:52:30 +0800

    add new module: Bm and Bmlb

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Bm.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Bm : Base
+    {
+        public Bm()
+        {
+            serverHost = "bm.api.qcloud.com";
+        }
+    }
+}
---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Bmlb.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Bmlb : Base
+    {
+        public Bmlb()
+        {
+            serverHost = "bmlb.api.qcloud.com";
+        }
+    }
+}
