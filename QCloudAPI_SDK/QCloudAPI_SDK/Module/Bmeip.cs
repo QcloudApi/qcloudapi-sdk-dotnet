b482f817b27a610bcacf717c37cc255a30f886e9
jimmyzhuang <jimmyzhuang@tencent.com>
2017-09-11 10:55:21 +0800

    add new module: bmeip and bmvpc

---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Bmeip.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Bmeip : Base
+    {
+        public Bmeip()
+        {
+            serverHost = "bmeip.api.qcloud.com";
+        }
+    }
+}
---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Bmvpc.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QCloudAPI_SDK.Module
+{
+    class Bmvpc : Base
+    {
+        public Bmvpc()
+        {
+            serverHost = "bmvpc.api.qcloud.com";
+        }
+    }
+}
