484845f86919511125f358fae04caa12398dc0e8
ZhiQiang Fan <zhiqiangfan@tencent.com>
2018-03-02 05:47:39 -0800

    add tbaas module

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -5,6 +5,7 @@ CHANGELOG
 latest (now)
 ============
 
+* add tbaas module
 * add athena module
 * add batch module
 * add partners module
---
+++ QCloudAPI_SDK/QCloudAPI_SDK/Module/Tbaas.cs
@@ -0,0 +1,10 @@
+namespace QCloudAPI_SDK.Module
+{
+    public class Tbaas : Base
+    {
+        public Tbaas()
+        {
+            serverHost = "tbaas.api.qcloud.com";
+        }
+    }
+}
